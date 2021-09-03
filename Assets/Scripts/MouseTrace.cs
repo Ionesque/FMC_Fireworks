254185c454ff571d72c6e62aeb7c519775790d39
FuzzyFish <ionesqueish@gmail.com>
2021-09-02 20:05:43 -0400

    Mouse held function added
    Added function to detect time held and generate repeats

--- Assets/Scripts/MouseTrace.cs
+++ Assets/Scripts/MouseTrace.cs
@@ -9,6 +9,42 @@ public class MouseTrace : MonoBehaviour
     public GameObject shell;
     Transform t;
 
+    float[] heldTime = new float[8]
+    {
+        0.0f,
+        0.0f,
+        0.0f,
+        0.0f,
+        0.0f,
+        0.0f,
+        0.0f,
+        0.0f
+    };
+
+    float[] refireTime = new float[8]
+    {
+        0.25f,
+        0.25f,
+        0.25f,
+        0.25f,
+        0.25f,
+        0.25f,
+        0.25f,
+        0.25f
+    };
+
+    bool[] fired = new bool[8]
+    {
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false
+    };
+
     private void Start()
     {
         cam = GetComponent<Camera>();
@@ -23,20 +59,37 @@ public class MouseTrace : MonoBehaviour
         // Bit shift the index of the layer (8) to get a bit mask
         int layerMask = 1 << 8;
 
+
         // This would cast rays only against colliders in layer 8.
         // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
         layerMask = ~layerMask;
 
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1"))
         {
             Vector3 mousePos = Input.mousePosition;
             {
                 //Debug.Log(mousePos.x);
                 //Debug.Log(mousePos.y);
                 Physics.Raycast(ray, out hit, 100f, layerMask);
-                Debug.Log(hit.point);
-                Instantiate(shell, hit.point, Quaternion.identity);
+                if(!fired[0]) Instantiate(shell, hit.point, Quaternion.identity);
+                fired[0] = true;
+                heldTime[0] += Time.deltaTime;
+                Debug.Log(heldTime[0]);
+                if(heldTime[0] > 0.25f)
+                {
+                    refireTime[0] -= Time.deltaTime;
+                    if (refireTime[0] < 0.0f)
+                    {
+                        Instantiate(shell, hit.point, Quaternion.identity);
+                        refireTime[0] = Random.Range(0.15f, 0.40f);
+                    }
+                }
             }
+        }
+        else
+        {
+            heldTime[0] = 0.0f;
+            fired[0] = false;
         }
         
         //fingerPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position); //Mobile
@@ -46,25 +99,6 @@ public class MouseTrace : MonoBehaviour
     // See Order of Execution for Event Functions for information on FixedUpdate() and Update() related to physics queries
     void FixedUpdate()
     {
-        // Bit shift the index of the layer (8) to get a bit mask
-        int layerMask = 1 << 8;
-
-        // This would cast rays only against colliders in layer 8.
-        // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
-        layerMask = ~layerMask;
-
-        RaycastHit hit;
-        // Does the ray intersect any objects excluding the player layer
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
-        {
-            //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-            //Debug.Log("Did Hit");
-            //Debug.Log(fingerPos);
-        }
-        else
-        {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
-            Debug.Log("Did not Hit");
-        }
+        
     }
 }
