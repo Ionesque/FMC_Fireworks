c3da0966f984dc16f14d039cea9695f18916d802
FuzzyFish <ionesqueish@gmail.com>
2021-09-03 03:45:21 -0400

    Added rainbow variable
    Not implemented yet, cycles between specific colors and full on rainbows

--- Assets/Scripts/Global.cs
+++ Assets/Scripts/Global.cs
@@ -7,6 +7,8 @@ using UnityEngine;
 /// </summary>
 public class Global : MonoBehaviour
 {
+    public bool rainbow_mode = false;
+    
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/Spark.cs
+++ Assets/Scripts/Spark.cs
@@ -10,14 +10,50 @@ public class Spark : MonoBehaviour {
     float gravity = 3.0f;
     Color c;
 
-    
+    public Global g;
 
 	// Use this for initialization
 	void Start () {
         Lookup l = GameObject.Find("~Lookup Table").GetComponent<Lookup>();
         r = GetComponent<Renderer>();
         sparkVelocity = l.GenVelocity() * Random.Range(0.9f, 1.1f); ;
-        c = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), 1.0f);
+        int rand_c = (int)Random.Range(0.0f, 7.0f);
+        if (rainbow)
+        {
+            switch (rand_c)
+            {
+                case (0):
+                    c = new Color(Random.Range(0.5f, 1.0f), 0.0f, 0.0f);
+                    break;
+                case (1):
+                    c = new Color(Random.Range(0.5f, 1.0f), Random.Range(0.25f, 0.75f), 0.0f);
+                    break;
+                case (2):
+                    c = new Color(Random.Range(0.5f, 1.0f), Random.Range(0.5f, 1.0f), 0.0f);
+                    break;
+                case (3):
+                    c = new Color(0.0f, Random.Range(0.5f, 1.0f), 0.0f);
+                    break;
+                case (4):
+                    c = new Color(0.0f, 0.0f, Random.Range(0.5f, 1.0f));
+                    break;
+                case (5):
+                    c = new Color(0.0f, Random.Range(0.5f, 1.0f), Random.Range(0.5f, 1.0f));
+                    break;
+                case (6):
+                    c = new Color(Random.Range(0.5f, 1.0f), 0.0f, Random.Range(0.5f, 1.0f));
+                    break;
+                default:
+                    Debug.Log("ERROR: Invalid color picked out of range");
+                    break;
+            }
+        }
+        else
+        {
+            c = new Color(Random.Range(0.5f, 1.0f), Random.Range(0.5f, 1.0f), Random.Range(0.5f, 1.0f));
+        }
+
+        
         r.material.color = c;
     }
 	
