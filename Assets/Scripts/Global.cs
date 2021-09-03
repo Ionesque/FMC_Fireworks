dda53e17b311fc538cdd48070ecf8b3026bc71c4
FuzzyFish <ionesqueish@gmail.com>
2021-09-02 20:01:49 -0400

    Enabled framerate limiter to 30 FPS
    Used to for better mobile performance

---
+++ Assets/Scripts/Global.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This will always be availiable regardless of scene
+/// </summary>
+public class Global : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 30;   // More time for calculations on mobile device target
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
